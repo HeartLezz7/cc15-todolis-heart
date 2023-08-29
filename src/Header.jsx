84c0223330c89f94b0dc55f872b3d10ac950a0d4
HeartLezz7 <heartchantapath@gmail.com>
2023-08-29 17:08:00 +0700

    finish:header

---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import { FaHome } from 'react-icons/fa';
+import "./Header.scss"
+import Search from "./Search";
+
+function Header() {
+    return(
+        <header className="header">
+        <div className="header__logo">
+          <FaHome />
+        </div>
+        <div className="header__text">
+          <h1>TodoList</h1>
+        </div>
+        <div className="header__search">
+        <Search/> 
+        </div>
+      </header>
+    )
+}
+
+export default Header;
